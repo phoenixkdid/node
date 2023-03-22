b0ece9b57d71b420bfaed56b1ccf3c833242cf5b
egooktafanda97 <egooktafanda1097@gmail.com>
2023-03-22 11:54:03 +0700

    ego oktafanda g

--- bk/routes/WaRouter.js
+++ bk/routes/WaRouter.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import WaController from '../controllers/WaController.js'
-import { verifyToken } from '../middlewares/VerifyToken.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
 import { verifyTokenSession } from '../middlewares/verifyTokenSession.js'
 // import { refreshToken } from '../controllers/RefreshToken.js'
 const RouterWaController = new WaController()
